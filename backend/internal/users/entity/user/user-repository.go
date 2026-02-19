accdf464ae62ef9e9e28e8ece90d51d6a2fb6eb8
Ilya Selyutin <leavetobefree777@gmail.com>
2026-02-19 20:49:10 +1000

    feat: add CRUD methods to user repository

--- backend/internal/users/entity/user/user-repository.go
+++ backend/internal/users/entity/user/user-repository.go
@@ -4,6 +4,11 @@ import "gorm.io/gorm"
 
 type UserRepositoryInterface interface {
 	GetByID(ID string) (User, error)
+	GetByPhone(phone string) (User, error)
+	GetAllUsers() ([]User, error)
+	Create(user User) (User, error)
+	Delete(ID string) error
+	Update(ID string, updates map[string]interface{}) (User, error)
 }
 
 type UserRepository struct {
@@ -25,4 +30,51 @@ func (r *UserRepository) GetByID(ID string) (User, error) {
 	}
 
 	return user, nil
+}
+
+func (r *UserRepository) GetByPhone(phone string) (User, error) {
+	var u User
+	result := r.db.First(&u, "phone = ?", phone)
+	if result.Error != nil {
+		return User{}, result.Error
+	}
+	return u, nil
+}
+
+func (r *UserRepository) GetAllUsers() ([]User, error) {
+
+	var users []User
+	result := r.db.Find(&users)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return users, nil
+}
+
+func (r *UserRepository) Create(u User) (User, error) {
+	result := r.db.Create(&u)
+	if result.Error != nil {
+		return User{}, result.Error
+	}
+	return u, nil
+}
+
+func (r *UserRepository) Delete(ID string) error {
+	result := r.db.Model(&User{}).Where("id = ?", ID).Update("is_active", false)
+	return result.Error
+}
+
+func (r *UserRepository) Update(ID string, updates map[string]interface{}) (User, error) {
+	var u User
+
+	if err := r.db.First(&u, "id = ?", ID).Error; err != nil {
+		return User{}, err
+	}
+
+	if err := r.db.Model(&u).Updates(updates).Error; err != nil {
+		return User{}, err
+	}
+
+	return u, nil
 }
